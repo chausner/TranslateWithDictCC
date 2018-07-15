c4fea75d24bdd7b585fc03dd761b5a47ca5f7220
Christoph Hausner <christoph_hausner@hotmail.de>
2018-07-15 12:32:14 +0200

    Revise about page

---
+++ TranslateWithDictCC/ViewModels/AboutViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Input;
+using Windows.ApplicationModel.Resources;
+using Windows.System;
+
+namespace TranslateWithDictCC.ViewModels
+{
+    class AboutViewModel : ViewModel
+    {
+        public string AppVersion { get; }
+
+        public ICommand GiveFeedbackCommand { get; }
+
+        public AboutViewModel()
+        {
+            ResourceLoader resourceLoader = ResourceLoader.GetForCurrentView();
+            
+            AppVersion = string.Format(resourceLoader.GetString("AboutPage_Version"), GetType().GetTypeInfo().Assembly.GetName().Version);
+
+            GiveFeedbackCommand = new RelayCommand(RunGiveFeedbackCommand);
+        }
+
+        private async void RunGiveFeedbackCommand()
+        {
+            await Launcher.LaunchUriAsync(new Uri("mailto:translatewithdict.cc@outlook.com"));
+        }
+    }
+}
--- TranslateWithDictCC/Views/AboutPage.xaml.cs
+++ TranslateWithDictCC/Views/AboutPage.xaml.cs
@@ -1,26 +1,17 @@
-﻿using System;
-using System.Reflection;
-using Windows.ApplicationModel.Resources;
-using Windows.System;
-using Windows.UI.Xaml;
+﻿using TranslateWithDictCC.ViewModels;
 using Windows.UI.Xaml.Controls;
 
 namespace TranslateWithDictCC.Views
 {
     public sealed partial class AboutPage : Page
     {
+        AboutViewModel ViewModel { get; }
+
         public AboutPage()
         {
             InitializeComponent();
 
-            ResourceLoader resourceLoader = ResourceLoader.GetForCurrentView();
-
-            versionTextBlock.Text = string.Format(resourceLoader.GetString("AboutPage_Version"), GetType().GetTypeInfo().Assembly.GetName().Version);
-        }
-
-        private async void Button_Click(object sender, RoutedEventArgs e)
-        {
-            await Launcher.LaunchUriAsync(new Uri("mailto:translatewithdict.cc@outlook.com"));
+            ViewModel = new AboutViewModel();
         }
     }
 }
