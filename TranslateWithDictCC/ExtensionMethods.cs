d3c3bcd179e29d41b1c78371e937c2b5fa1ef67f
Christoph Hausner <christoph_hausner@hotmail.de>
2022-01-03 22:50:52 +0100

    Remove obsolete DistinctBy extension method

--- TranslateWithDictCC/ExtensionMethods.cs
+++ TranslateWithDictCC/ExtensionMethods.cs
@@ -13,30 +13,5 @@ namespace TranslateWithDictCC
             if (frame.SourcePageType != sourcePageType)
                 frame.Navigate(sourcePageType, parameter, transitionInfo);
         }
-
-        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> enumerable, Func<TSource, TKey> selector)
-        {
-            return enumerable.Distinct(new DistinctEqualityComparer<TSource, TKey>(selector));
-        }
-
-        private class DistinctEqualityComparer<TSource, TKey> : EqualityComparer<TSource>
-        {
-            Func<TSource, TKey> selector;
-
-            public DistinctEqualityComparer(Func<TSource, TKey> selector)
-            {
-                this.selector = selector;
-            }
-
-            public override bool Equals(TSource x, TSource y)
-            {
-                return selector(x).Equals(selector(y));
-            }
-
-            public override int GetHashCode(TSource obj)
-            {
-                return selector(obj).GetHashCode();
-            }
-        }
     }
 }
