de208e87c934afa4077736f6c5ee21844a69043e
Christoph Hausner <christoph_hausner@hotmail.de>
2026-03-28 23:31:44 +0100

    Use extension block

--- TranslateWithDictCC/ExtensionMethods.cs
+++ TranslateWithDictCC/ExtensionMethods.cs
@@ -6,9 +6,12 @@ namespace TranslateWithDictCC;
 
 static class ExtensionMethods
 {
-    public static void NavigateIfNeeded(this Frame frame, Type sourcePageType, object parameter = null, NavigationTransitionInfo transitionInfo = null)
+    extension(Frame frame)
     {
-        if (frame.SourcePageType != sourcePageType)
-            frame.Navigate(sourcePageType, parameter, transitionInfo);
+        public void NavigateIfNeeded(Type sourcePageType, object parameter = null, NavigationTransitionInfo transitionInfo = null)
+        {
+            if (frame.SourcePageType != sourcePageType)
+                frame.Navigate(sourcePageType, parameter, transitionInfo);
+        }
     }
 }
