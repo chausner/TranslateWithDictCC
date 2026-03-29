942106d48881063ee8dccc3d3f07ef9702bd5009
Christoph Hausner <christoph_hausner@hotmail.de>
2026-03-29 20:55:23 +0200

    Remove comments

--- TranslateWithDictCC/MainWindow.xaml.cs
+++ TranslateWithDictCC/MainWindow.xaml.cs
@@ -11,14 +11,8 @@ using Windows.UI;
 using WinRT.Interop;
 using WinUIEx;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace TranslateWithDictCC;
 
-/// <summary>
-/// An empty window that can be used on its own or navigated to within a Frame.
-/// </summary>
 public sealed partial class MainWindow : WindowEx
 {
     public static MainWindow Instance { get; private set; } = null!;
