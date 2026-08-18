fce53b59a477e5bc15e2fdf2c728b078334173ed
Christoph Hausner <christoph_hausner@hotmail.de>
2026-08-19 00:03:24 +0200

    Enable XAML optional changes for improved performance

---
+++ TranslateWithDictCC/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Settings;
+using System.Threading;
+
+namespace TranslateWithDictCC;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        XamlOptionalChanges.EnableChange(XamlChangeId.DefaultStyleOptimizations);
+        XamlOptionalChanges.EnableChange(XamlChangeId.OptimizeApplyStyles);
+        XamlOptionalChanges.EnableChange(XamlChangeId.IconNoGridOptimization);
+
+        WinRT.ComWrappersSupport.InitializeComWrappers();
+        Application.Start((p) => {
+            var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
+            SynchronizationContext.SetSynchronizationContext(context);
+            new App();
+        });
+    }
+}
